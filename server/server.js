980e2880997b6de331a32911c8a14503f84769ca
Kyle Saburao <ksaburao@sfu.ca>
2021-07-01 10:32:29 -0700

    Test Docker build trigger

--- server/server.js
+++ server/server.js
@@ -2,9 +2,11 @@ const express = require("express");
 const path = require("path");
 const app = express();
 
+const PORT = 8888;
+
 app.use(express.static(path.join(__dirname, "dist", "connect4web")));
 app.get("/*", (req, res) => {
   res.sendFile(path.resolve(__dirname, "dist", "connect4web", "index.html"));
 });
 
-app.listen("8888", () => console.log("Running"));
+app.listen(`${PORT}`, () => console.log("Running"));
